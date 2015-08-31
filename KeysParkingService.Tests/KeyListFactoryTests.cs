6be62a947c0edbc6074ed86135950834c9fee84c
shaposhnikov <mr.shaposhnikov@mail.ru>
2015-08-31 23:30:55 +0600

    - [ADD] Added unit tests project

---
+++ KeysParkingService.Tests/KeyListFactoryTests.cs
@@ -0,0 +1,45 @@
+﻿using KeysParkingService.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeysParkingService.Tests
+{
+    [TestFixture]
+    public class KeyListFactoryTests
+    {
+        [Test]
+        public void SetKeyList_EmptyList_ReturnsEmptyList()
+        {
+            // arrange
+            var factory = new KeyListFactory();
+            factory.SetKeyList(new List<Key>());
+
+            // act
+            var emptyList = factory.Create();
+
+            // assert
+            Assert.IsTrue(emptyList.Count == 0);
+        }
+
+        [Test]
+        public void SetKeyList_List_AlwaysReturnsEqualList()
+        {
+            var key1 = new Key() { Id = 1 };
+            var key2 = new Key() { Id = 2 };
+            var key3 = new Key() { Id = 3 };
+            var factory = new KeyListFactory();
+            factory.SetKeyList(new List<Key>() { key1, key2, key3 });
+
+            var list = factory.Create().ToList();
+
+            Assert.Contains(key1, list);
+            Assert.Contains(key2, list);
+            Assert.Contains(key3, list);
+            Assert.True(list.Count == 3);
+        }
+    }
+}
