816e89f535e7f14fca0f7e9b44ba40bc47ed3765
shaposhnikov <mr.shaposhnikov@mail.ru>
2015-09-30 13:23:54 +0600

    - [ADD] Added Key class as other file - [ADD] Added fields "CreateTime" and "SiteLink"

---
+++ KeysParkingService/Models/Key.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace KeysParkingService.Models
+{
+
+    [Table("keys", Schema = "public")]
+    public class Key
+    {
+        [Key]
+        [Column("id")]
+        public int Id
+        { get; set; }
+
+        [Column("create_time")]
+        public DateTime CreateTime
+        { get; set; }
+
+        [Column("link")]
+        public string SiteLink
+        { get; set; }
+
+        [Column("login")]
+        public string Login
+        { get; set; }
+
+        [Column("password")]
+        public string Password
+        { get; set; }
+    }
+}
